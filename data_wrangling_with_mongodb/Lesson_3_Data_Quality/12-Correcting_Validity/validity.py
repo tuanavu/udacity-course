e1a59e7b56b8c39350eaff09ab104ccbf5e4b41d
Tuan Vu <tuanvu216@yahoo.com>
2018-03-07 23:49:28 -0800

    update validity code

--- data_wrangling_with_mongodb/Lesson_3_Data_Quality/12-Correcting_Validity/validity.py
+++ data_wrangling_with_mongodb/Lesson_3_Data_Quality/12-Correcting_Validity/validity.py
@@ -24,21 +24,40 @@ OUTPUT_GOOD = 'autos-valid.csv'
 OUTPUT_BAD = 'FIXME-autos.csv'
 
 def process_file(input_file, output_good, output_bad):
-
+    # store data into lists for output
+    data_good = []
+    data_bad = []
     with open(input_file, "r") as f:
         reader = csv.DictReader(f)
         header = reader.fieldnames
+        for row in reader:
+            # validate URI value
+            if row['URI'].find("dbpedia.org") < 0:
+                continue
 
-        #COMPLETE THIS FUNCTION
+            ps_year = row['productionStartYear'][:4]
+            try: # use try/except to filter valid items
+                ps_year = int(ps_year)
+                row['productionStartYear'] = ps_year
+                if (ps_year >= 1886) and (ps_year <= 2014):
+                    data_good.append(row)
+                else:
+                    data_bad.append(row)
+            except ValueError: # non-numeric strings caught by exception
+                if ps_year == 'NULL':
+                    data_bad.append(row)
 
-
-
-    # This is just an example on how you can use csv.DictWriter
-    # Remember that you have to output 2 files
-    with open(output_good, "w") as g:
-        writer = csv.DictWriter(g, delimiter=",", fieldnames= header)
+    # Write processed data to output files
+    with open(output_good, "w") as good:
+        writer = csv.DictWriter(good, delimiter=",", fieldnames= header)
         writer.writeheader()
-        for row in YOURDATA:
+        for row in data_good:
+            writer.writerow(row)
+
+    with open(output_bad, "w") as bad:
+        writer = csv.DictWriter(bad, delimiter=",", fieldnames= header)
+        writer.writeheader()
+        for row in data_bad:
             writer.writerow(row)
 
 
--- data_wrangling_with_mongodb/README.md
+++ data_wrangling_with_mongodb/README.md
@@ -10,6 +10,10 @@ This repo contains lectures and assignments of [Data Wrangling with MongoDB - Ud
 | Lecture | Description |
 |--------------------------------------------------------------------------------------------------------------|-------------------------------------------------------------------------------------------------------------------------------------------------------------------|
 | [Lecture 1](./Lesson_1_Data_Extraction_Fundamentals)|[Problem Set 1](./Lesson_1_Problem_Set) | Data Extraction Fundamentals - Working with CSV, Excel, JSON file |
+| [Lecture 2](./Lesson_2_Data_in_More_Complex_Formats)|[Problem Set 2](./Lesson_2_Problem_Set) | Data in more complex format - XML and web pages |
+| [Lecture 3](./Lesson_3_Data_Quality)|[Problem Set 3](./Lesson_3_Problem_Set) | Data Quality |
+| [Lecture 4](./Lesson_4_Working_with_MongoDB)|[Problem Set 4](./Lesson_4_Problem_Set) | Working with MongoDB |
+| [Lecture 5](./Lesson_5_Analyzing_Data)|[Problem Set 5](./Lesson_5_Problem_Set) | Analyzing data in MongoDB |
 
 
 ## License
